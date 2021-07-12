55124d61c75aeebedf0f5b48988c1817f482adf7
sarahchoe01 <52465156+sarahchoe01@users.noreply.github.com>
2021-07-12 10:42:04 -0600

    Update blog.js

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,7 +22,30 @@ class Blog extends Component {
     window.addEventListener("scroll", this.onScroll, false);
     this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
     this.handleModalClose = this.handleModalClose.bind(this);
-    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(this);
+    this.handleSuccessfulNewBlogSubmission = this.handleSuccessfulNewBlogSubmission.bind(
+      this
+    );
+    this.handleDeleteClick = this.handleDeleteClick.bind(this);
+  }
+
+  handleDeleteClick(blog) {
+    axios
+      .delete(
+        `https://api.devcamp.space/portfolio/portfolio_blogs/${blog.id}`,
+        { withCredentials: true }
+      )
+      .then(response => {
+        this.setState({
+          blogItems: this.state.blogItems.filter(blogItem => {
+            return blog.id !== blogItem.id;
+          })
+        });
+
+        return response.data;
+      })
+      .catch(error => {
+        console.log("delete blog error", error);
+      });
   }
 
   handleSuccessfulNewBlogSubmission(blog) {
@@ -67,8 +90,9 @@ class Blog extends Component {
 
     axios
       .get(
-        `https://sarahchoe.devcamp.space/portfolio/portfolio_blogs?page=${this
-          .state.currentPage}`,
+        `https://sarahchoe.devcamp.space/portfolio/portfolio_blogs?page=${
+          this.state.currentPage
+        }`,
         {
           withCredentials: true
         }
@@ -96,24 +120,37 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map(blogItem => {
-      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      if (this.props.loggedInStatus === "LOGGED_IN") {
+        return (
+          <div key={blogItem.id} className="admin-blog-wrapper">
+            <BlogItem blogItem={blogItem} />
+            <a onClick={() => this.handleDeleteClick(blogItem)}>
+              <FontAwesomeIcon icon="trash" />
+            </a>
+          </div>
+        );
+      } else {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+      }
     });
 
     return (
       <div className="blog-container">
         <BlogModal
-          handleSuccessfulNewBlogSubmission = {this.handleSuccessfulNewBlogSubmission}
+          handleSuccessfulNewBlogSubmission={
+            this.handleSuccessfulNewBlogSubmission
+          }
           handleModalClose={this.handleModalClose}
           modalIsOpen={this.state.blogModalIsOpen}
         />
 
         {this.props.loggedInStatus === "LOGGED_IN" ? (
-          <div className = "new-blog-link">
-            <a onClick = {this.handleNewBlogClick}>
+          <div className="new-blog-link">
+            <a onClick={this.handleNewBlogClick}>
               <FontAwesomeIcon icon="plus-circle" />
             </a>
           </div>
-        ):null}
+        ) : null}
 
         <div className="content-container">{blogRecords}</div>
 
