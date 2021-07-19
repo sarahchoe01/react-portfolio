61f73f1373aa451a9d6fe63457cfb9e15a1e7e1f
sarahchoe01 <52465156+sarahchoe01@users.noreply.github.com>
2021-07-19 10:05:42 -0600

    Update about.js

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react";
-import profilePicture from "../../../static/assets/images/bio/headshot.JPG";
+import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
 
 export default function() {
     return (
